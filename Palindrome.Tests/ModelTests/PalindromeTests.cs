070428918562e0aef9a6a6c75bbc440dbad90d43
Albert Lee <leesga8@gmail.com>
2021-12-07 13:56:18 -0800

    initial folders
    Co-authored-by: Karen Axon <kaxonwa@gmail.com>

---
+++ Palindrome.Tests/ModelTests/PalindromeTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Palindrome;
+
+namespace Palindrome.Tests
+{
+  [TestClass]
+  public class InputCheckerTests
+  {
+    [TestMethod]
+    public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+    {
+      // any necessary logic to prep for test; instantiating new classes, etc.
+      // Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+    }
+  }
+}
---
+++ Palindrome/Models/Palindrome.cs
@@ -0,0 +1,7 @@
+namespace Palindrome
+{
+  public class InputChecker
+  {
+    // properties, constructors, methods, etc. go here
+  }
+}
